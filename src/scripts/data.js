9017ac73f109085c0b403816accc18498daead32
Lacey Walker <65372269+laceywalkerr@users.noreply.github.com>
2020-06-22 10:11:12 -0500

    added new pages to js and added links to the html

---
+++ src/scripts/data.js
@@ -0,0 +1,15 @@
+const journalEntry = [
+    {
+    title: "array methods",
+    date: "06-22-20",
+    mood: "tired",
+    entry: "trying to learn about array methods still"
+},
+
+{
+    title: "array methods",
+    date: "06-22-20",
+    mood: "tired",
+    entry: "trying to learn about array methods still"
+}
+]
---
+++ src/scripts/entryList.js
@@ -0,0 +1,11 @@
+/*
+    Purpose: To render all journal entries to the DOM
+
+    Arguments: entries (array of objects)
+*/
+const renderJournalEntries = (entries) => {
+
+}
+
+// Invoke the render function
+renderJournalEntries(journalEntry)
